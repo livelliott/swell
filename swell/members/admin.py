bf252f4c41bb98e146a85df83cc15772aea818f1
Liv Elliott <96855754+livelliott@users.noreply.github.com>
2023-11-09 22:22:34 -0500

    register member model to admin

--- swell/members/admin.py
+++ swell/members/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Member
 
-# Register your models here.
+# Register your models here - registered models can be accessed via admin panel
+class MemberAdmin(admin.ModelAdmin):
+  list_display = ("firstname", "lastname", "joined_date",)
+  
+admin.site.register(Member, MemberAdmin)
--- swell/members/views.py
+++ swell/members/views.py
@@ -30,8 +30,9 @@ def main(request):
   return HttpResponse(template.render())
 
 def testing(request):
+  mydata = Member.objects.values()
   template = loader.get_template('template.html')
   context = {
-    'fruits': ['Apple', 'Banana', 'Cherry'],   
+    'mymembers': mydata,
   }
   return HttpResponse(template.render(context, request))
