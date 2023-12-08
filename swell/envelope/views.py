13834a8fde8db41cc11138955fbd5b1032361ea1
Liv Elliott <96855754+livelliott@users.noreply.github.com>
2023-12-07 23:21:36 -0500

    send invites template

--- swell/envelope/views.py
+++ swell/envelope/views.py
@@ -8,8 +8,6 @@ from django.contrib.auth.models import User
 from swell.constants import EMAIL_PATTERN
 from invitations.utils import get_invitation_model
 from .forms import EnvelopeForm
-from django.utils import timezone
-from datetime import datetime, time
 import re
 
 @login_required
@@ -18,23 +16,10 @@ def envelope_create(request):
         form = EnvelopeForm(request.POST)
         if form.is_valid():
             envelope_form = form.save(commit=False)
-            members = form.cleaned_data['members']
+            # envelope_id = form.cleaned_data['envelope_id']
             envelope_name = form.cleaned_data['envelope_name']
-            # partition invitees
-            split_members = [member.strip() for member in members.split(' ')]
-            # send invite to each
-            for member in split_members:
-                if valid_invite(member):
-                    pass
-                    # send invite
-                else:
-                    pass
-                    # save and display could not send invite message
-                pass
-            # create envelope instance
-            envelope = Envelope.objects.create(envelope_name=envelope_name)
-            group = Group.ojects.create(group_name=envelope_name)
-            # save to database
+            members = form.cleaned_data['members']
+            invite_members(request, members)
             envelope_form.save()
             return redirect(reverse('envelope:envelope_create_success'))
     else:
@@ -51,10 +36,23 @@ def valid_invite(user):
     # if valid email address
     if re.fullmatch(EMAIL_PATTERN, user):
         return user
-    # if valid username in user database
-    username = User.objects.filter(username=user).first()
-    if not username == None:
-        # retrieve and return valid email
-        user_found = get_object_or_404(User, username=user)
-        return user_found.email
-    return None
+    else: # if valid username in user database
+      try:
+        user_instance = User.objects.get(username=user)
+        return user_instance.email
+      except User.DoesNotExist:
+        # Handle the case where the user with the given username does not exist
+        return None
+
+def invite_members(request, members):
+    split_members = [member.strip() for member in members.split(' ')]
+    for member in split_members:
+        user_email = valid_invite(member)
+        if user_email:
+            messages.success(request, "Invite sent to " + user_email)
+            # send invite
+        else:
+            messages.success(request, "ERROR")
+            # save and display could not send invite message
+        pass
+    pass
