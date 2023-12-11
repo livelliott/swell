c68ca784dcfd6c7b6817ea2bba00cab63a949b1d
Liv Elliott <96855754+livelliott@users.noreply.github.com>
2023-12-11 14:35:07 -0500

    drop down menu for due date

--- swell/envelope/forms.py
+++ swell/envelope/forms.py
@@ -1,13 +1,21 @@
 from django import forms
 # from board.models import Invitation, Group, UserGroup
 from .models import Envelope
+from datetime import datetime, timedelta
 
 class EnvelopeForm(forms.ModelForm):
+    current_date = datetime.now()
+    DUE_DATE_CHOICES = [
+        ((current_date + timedelta(days=14)).strftime("%Y-%m-%d"), 'Every two weeks'),
+        ((current_date + timedelta(days=30)).strftime("%Y-%m-%d"), 'Every month'),
+        ((current_date + timedelta(days=60)).strftime("%Y-%m-%d"), 'Every two months'),
+        ((current_date + timedelta(days=180)).strftime("%Y-%m-%d"), 'Every six months'),
+    ]
+    envelope_due_date = forms.ChoiceField(choices=DUE_DATE_CHOICES, required=False)
     class Meta:
         model = Envelope
         fields = ["envelope_name", "envelope_query_date", "envelope_due_date"]
         widgets = {
             'envelope_query_date': forms.DateInput(attrs={'type': 'date'}),
-            'envelope_due_date': forms.DateInput(attrs={'type': 'date'}),
         }
     members = forms.CharField(max_length=1000, required=True)
