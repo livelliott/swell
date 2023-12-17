f93492694fa181a993e619b50c1de3e2b29ef529
Liv Elliott <96855754+livelliott@users.noreply.github.com>
2023-12-16 23:16:15 -0500

    display joined evelopes

--- swell/board/views.py
+++ swell/board/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.template import loader
-from .models import Invitation, UserGroup
+from .models import Invitation, UserGroup, Group
 from envelope.models import Envelope
 from django.core.mail import send_mail
 from django.conf import settings
@@ -16,7 +16,7 @@ def home_page(request):
     # Assuming you have a user object
     user = request.user
     created_by = Envelope.objects.filter(envelope_admin=user)
-    member_of = UserGroup.objects.filter(user=user)
+    member_of = get_joined_envelopes(request)
     invited_to = Invitation.objects.filter(recipient=user)
     context = {
         'user': user,
@@ -25,3 +25,10 @@ def home_page(request):
         'invited_to': invited_to,
     }
     return render(request, 'home.html', context)
+
+def get_joined_envelopes(request):
+    joined_envelopes = []
+    user_groups = UserGroup.objects.filter(user=request.user)
+    for user_group in user_groups:
+        joined_envelopes.append(user_group.group.envelope)
+    return joined_envelopes
