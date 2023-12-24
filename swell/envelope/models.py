e542bf57602bb790656c0c8c426e8c15bad20d8a
Liv Elliott <96855754+livelliott@users.noreply.github.com>
2023-12-23 21:42:52 -0500

    modify to display start date

--- swell/envelope/models.py
+++ swell/envelope/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from datetime import timedelta
 from question.models import DefaultQuestion, AdminQuestion, UserQuestion
 
 # created by a user, sent to other users
@@ -8,20 +9,25 @@ class Envelope(models.Model):
     envelope_id = models.AutoField(primary_key=True)
     envelope_name = models.CharField(max_length=200)
     envelope_admin = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
-    envelope_query_date = models.DateField(default=(timezone.now() + timezone.timedelta(days=2)).date(), null=True)
+    envelope_query_date = models.DateField(default=(timezone.now() + timezone.timedelta(days=2)).astimezone(timezone.get_current_timezone()).date(), null=True)
     envelope_due_date = models.DateField(blank=True, null=True)
     questions_default = models.ManyToManyField(DefaultQuestion, blank=True)
     questions_admin = models.ManyToManyField(AdminQuestion, blank=True)
     questions_user = models.ManyToManyField(UserQuestion, blank=True)
+
     @property
     def delivery_date(self):
-        if self.envelope_due_date:
-            today = timezone.now().date()
-            remaining_days = max(0, (self.envelope_due_date - today).days)
-            if remaining_days == 0:
-                return "Delivers today"
-            elif remaining_days == 1:
-                return "Delivers tomorrow"
-            else:
-                return f"Delivers in {remaining_days} days"
+        today = timezone.localdate()
+        start_date = (self.envelope_query_date - today).days
+        remaining_days = max(0, (self.envelope_due_date - today).days)
+        if start_date == 1:
+            return f"Starts tomorrow"
+        elif start_date > 1:
+            return f"Starts in {start_date} days"
+        elif remaining_days == 0:
+            return "Delivers today"
+        elif remaining_days == 1:
+            return "Delivers tomorrow"
+        else:
+            return f"Delivers in {remaining_days} days"
         return None
